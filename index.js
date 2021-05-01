8fd4e67fa2cf51c18052d1d606717f828fecfd8c
michael 2 <trujillom9@gmail.com>
2021-04-30 18:28:17 -0700

    added initial files and started on index.html and index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+let counter1 = 0, counter2 = 0, turn = true;
+//true = counter1(player 1 turn)
+//false = counter2(player 2 turn)
+
+
+
+// create a dice roll that will...
+//  zero out the opposite counter each roll
+//  toggle the turn
+
+//create a case scenario for button tile clicks only able to click the button if display is none (tile is not already chosen)
+// put a switch inside of an if statement with conditionof (display =none)
+
+// if(display = none){
+//     (counter1>0){
+//         display "x";
+//         counter1--;
+//     }
+
+//     (counter2>0){
+//         display "o";
+//         counter1--;
+//     }
+// }
+
+
+
